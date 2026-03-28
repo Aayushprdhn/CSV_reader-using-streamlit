22dc132d2fca8c72264c7bf5545ad255eb5f70ed
Aayush <pradhanaayush289@gmail.com>
2026-03-28 14:44:19 +0545

    Updating unfinished code

---
+++ CSV_reader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+df = pd.read_csv("Student.csv")
+st.title("Student CSV Reader")
+st.subheader("All Student Data")
+st.dataframe(df)
+st.subheader("Search for a Student")
+student_name = st.text_input("Enter the students's name: ")
+if student_name:
+    result = df[df["Name"].str.strip().str.lower() == student_name.strip().lower()]
+    if not result.empty:
+        st.write("Here is the data for", student_name)
+    else:
+        st.warning("No student found with that name")
